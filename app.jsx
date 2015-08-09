e7cb72d02c86c27d26d21e6c7f9f94fa7a276309
Christian Ramsey <thechristianramsey@gmail.com>
2015-08-08 21:13:28 -0400

    added style updates and reduced markup

--- app.jsx
+++ app.jsx
@@ -42,6 +42,21 @@ var getCurrentTime = function() {
 
 };
 
+var allAboard = function() {
+//    get AppData and functionally add up all the times
+
+};
+
+var spanStyle = {
+    borderBottom: "1px dashed #007a73",
+    color: "#007a73",
+    padding: "6px",
+    marginLeft: "-6px",
+    marginRight: "-6px",
+    display: "inline-block",
+    position: "relative"
+}
+
 var SearchBar = React.createClass({
     getInitialState: function() {
         return {"rooms":getRooms()}
@@ -55,45 +70,13 @@ var SearchBar = React.createClass({
 
 
               <h5> {{ current_time }}  </h5>
-                I want to book
-              <span id="atmosphere" class="input-container">
-                <span class="placeholder"> any room</span>
-                <span class="input selectbox">
-                  <select class="fancy-select hidden" name="find_desc">
+                I want to book&nbsp;
+                <span style={spanStyle} class="placeholder input-container"> any room </span>
+                &nbsp;at&nbsp;
+                <span style={spanStyle}  class="placeholder input-container"> 10:00 AM </span>
+                &nbsp;for&nbsp;
+                <span style={spanStyle}  class="placeholder input-container"> 2 hours</span>
 
-
-                      <option value="any room">Any Room</option>
-                      <option value="The Cardigan Room">room #1 (5)</option>
-                      <option value="The Applegate Room">room #2 (2)</option>
-                      <option value="The Lofter Room">room #3 (5)</option>
-                      <option value="The Dungeoun Room">room #5 (2)</option>
-                  </select>
-                </span>
-              </span> at
-              <span id="cuisine" class="input-container">
-                <span class="placeholder"> 10:00 AM</span>
-                <span class="input selectbox">
-                  <select name="cflt" class="fancy-select hidden">
-                      <option value="11:00 AM">11:00 AM</option>
-                      <option value="12:00 AM">12:00 AM</option>
-                      <option value="13:00 AM">13:00 AM</option>
-                      <option value="14:00 AM">11:00 AM</option>
-                  </select>
-                </span>
-              </span>
-                            for
-
-               <span id="cuisine" class="input-container">
-                <span class="placeholder"> 2 hours</span>
-                <span class="input selectbox">
-                  <select name="cflt" class="fancy-select hidden">
-                      <option value="one hour">1 hour</option>
-                      <option value="two hours">2 hours</option>
-                      <option value="three hours">3 hours</option>
-                      <option value="4 hours">4 hours</option>
-                  </select>
-                </span>
-              </span>
             </form>
         );
     }
---
+++ rooms.jsx.js
@@ -0,0 +1,7 @@
+
+/**
+ * Created by christianramsey on 08/08/2015.
+ */
+<select name="" id="">
+    <option value="any room">Any Room</option>
+</select>
